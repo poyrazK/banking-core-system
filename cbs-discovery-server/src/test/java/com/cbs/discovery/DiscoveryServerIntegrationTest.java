800e675b028f76677af79c0c01a2cd5965a5575b
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-18 20:34:19 +0300

    test(cbs-discovery-server): add context integration test

---
+++ cbs-discovery-server/src/test/java/com/cbs/discovery/DiscoveryServerIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.cbs.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
