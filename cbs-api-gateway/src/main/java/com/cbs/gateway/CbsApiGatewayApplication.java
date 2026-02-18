67423fd986f8871391e84eb890ada2da52319d6d
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-18 20:29:19 +0300

    chore: add cbs-api-gateway/src/main/java/com/cbs/gateway/CbsApiGatewayApplication.java

---
+++ cbs-api-gateway/src/main/java/com/cbs/gateway/CbsApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.cbs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbsApiGatewayApplication.class, args);
+    }
+}
