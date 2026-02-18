81b8f0698c5945bcbc7a4e21b07ed419029bfa25
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-18 20:29:20 +0300

    chore: add cbs-card-service/src/main/java/com/cbs/card/CbsCardServiceApplication.java

---
+++ cbs-card-service/src/main/java/com/cbs/card/CbsCardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cbs.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbsCardServiceApplication.class, args);
+    }
+}
