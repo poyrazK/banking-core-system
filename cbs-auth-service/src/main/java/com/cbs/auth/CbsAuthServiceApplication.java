1d8d812b5824faeabf67d9d1dc516203f243f006
Poyraz Küçükarslan <83272398+PoyrazK@users.noreply.github.com>
2026-02-18 20:29:19 +0300

    chore: add cbs-auth-service/src/main/java/com/cbs/auth/CbsAuthServiceApplication.java

---
+++ cbs-auth-service/src/main/java/com/cbs/auth/CbsAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cbs.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbsAuthServiceApplication.class, args);
+    }
+}
